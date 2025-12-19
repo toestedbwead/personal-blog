0bfc9ef566f110de5762c390022ae9f427e09832
toestedbwead <leielcano@gmail.com>
2025-12-19 21:44:44 +0800

    URLs for Guest Article Detail and List View Done

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from blog.views import RegistrationView,LoginView
+from blog.views import RegistrationView,LoginView, ArticleListView, ArticleDetailView
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
+    # AUTH URLs
     path('register/', RegistrationView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+
+    # GUESTS URLs
+    path('articles/', ArticleListView.as_view(), name='home'),
+    path('articles/<slug:slug>', ArticleDetailView.as_view(), name='article-detail')
 ]
