97743c166ba716947f5ce2c45da2115d669cf1fc
toestedbwead <leielcano@gmail.com>
2025-12-19 21:36:19 +0800

    Added Article ListView and DetailView for displaying Articles as list and as one single article

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,9 @@ from django.contrib.auth import authenticate, login
 from django.urls import reverse_lazy
 from blog.forms import RegistrationForm
 from django.contrib.auth.views import LogoutView
+from django.views.generic import ListView
+from django.views.generic import DetailView
+from blog.models import Article
 
 # registration view 
 class RegistrationView(FormView):
@@ -28,3 +31,24 @@ class LoginView(FormView):
         user = form.get_user()
         login(self.request, user)
         return super().form_valid(form)
+    
+# article list view | homepage
+class ArticleListView(ListView):
+    model = Article
+    template_name = 'blog/guest/article_list.html'
+    context_object_name = 'articles'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return Article.objects.filter(is_published=True).order_by('-created_at')
+
+# article detail view | single article
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'blog/guest/article_detail.html'
+    context_object_name = 'article'
+    slug_field ='slug'
+    slug_url_kwarg = 'slug'
+
+    def get_queryset(self):
+        return Article.objects.filter(is_published=True)
