b4afe98a6eb492ba5463e6c4fc2c64fcf3c5154d
toestedbwead <leielcano@gmail.com>
2025-12-20 19:40:02 +0800

    URLs for CRUD views done.

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from blog.views import RegistrationView,LoginView, ArticleListView, ArticleDetailView
+from blog.views import (
+    RegistrationView, LoginView, ArticleListView, ArticleDetailView, ArticleCreateView, ArticleUpdateView, ArticleDeleteView, UserDashboardView)
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
@@ -10,5 +11,13 @@ urlpatterns = [
 
     # GUESTS URLs
     path('articles/', ArticleListView.as_view(), name='home'),
-    path('articles/<slug:slug>', ArticleDetailView.as_view(), name='article-detail')
+    path('articles/<slug:slug>/', ArticleDetailView.as_view(), name='article-detail'),
+
+    # CRUD URLs
+    path('articles/create/', ArticleCreateView.as_view(), name='article-create'),
+    path('articles/<slug:slug>/edit/', ArticleUpdateView.as_view(), name='article-edit'),
+    path('articles/<slug:slug>/delete/', ArticleDeleteView.as_view(), name='article-delete'),
+
+    # Dashboard
+    path('dashboard', UserDashboardView.as_view(), name='dashboard')
 ]
