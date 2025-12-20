93553813f452c6cb47fcac3534ebd922ffba48d4
toestedbwead <leielcano@gmail.com>
2025-12-20 11:06:48 +0800

    Article form done with auto generated slugs.

--- blog/forms.py
+++ blog/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from blog.models import User
+from blog.models import Article
+from django.utils.text import slugify
 
 # Registration form
 class RegistrationForm(UserCreationForm):
@@ -19,3 +21,33 @@ class RegistrationForm(UserCreationForm):
             raise forms.ValidationError("This email is already registered.")
         return email
 
+# Article form
+class ArticleForm(forms.ModelForm):
+    class Meta:
+        model = Article
+        fields = ('title', 'content', 'category', 'is_published')
+        widgets = {
+            'title': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Article title'
+            }),
+            'content': forms.Textarea(attrs={
+                'class': 'form-control',
+                'placeholder': 'Write your article here...',
+                'rows': 10
+            }),
+            'category': forms.Select(attrs={
+                'class': 'form-control'
+            }),
+            'is_published': forms.CheckboxInput(attrs={
+                'class': 'form-check-input'
+            })
+        }
+
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+        instance.slug = slugify(self.cleaned_data['title'])
+
+        if commit:
+            instance.save()
+        return instance
