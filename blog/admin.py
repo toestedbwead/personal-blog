1d76caab3412f6eba881bf0badde26482b9a3c08
toestedbwead <leielcano@gmail.com>
2026-01-26 11:57:18 +0800

    Added categories of articles using superadmin of django

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from blog.models import Category, User, Article
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Article)
+admin.site.register(User)
